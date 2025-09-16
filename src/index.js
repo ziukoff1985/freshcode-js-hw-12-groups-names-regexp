7d5f28242749ce79383ac6f288da96876ccd541a
Borys Ziukov <ziukoff1985@gmail.com>
2025-09-16 12:45:00 +0300

    Add regExp and main function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const arrGroups = [
+    'FE2021-5',
+    'Fe2020-2',
+    'FD2021',
+    'fD2022-1',
+    'FE2020-3',
+    'FD2020/2',
+    'fe2021-3',
+    'FD2020$5',
+    'FE2008-52',
+    'Fm2008-23',
+    'FM2022-1',
+];
+
+const regExp = /^(FD|FM|FE|fd|fm|fe)20\d{2}(-\d+)?$/;
+
+function checkGroupsNames(array, regexp) {
+    array.forEach((group) => {
+        if (!regexp.test(group)) {
+            console.log(`Error ${group}`);
+            return;
+        }
+        console.log(`Success: ${group}`);
+    });
+}
+
+checkGroupsNames(arrGroups, regExp);
